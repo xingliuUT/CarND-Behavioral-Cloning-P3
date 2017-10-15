02e8b78a4fac787e04526f7c44fb879180f4d532
Xing Liu <xingliu@utexas.edu>
2017-10-15 00:14:50 -0500

    feat: apply cropping layer, build LeNet in Keras

--- model.py
+++ model.py
@@ -19,15 +19,25 @@ X_train = np.array(images)
 y_train = np.array(steer)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape = (160, 320, 3))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape = (160, 320, 3)))
+model.add(Cropping2D(cropping = ((70,25), (0,0))))
+model.add(Convolution2D(6, 5, 5, activation = "relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation = "relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
 model.fit(X_train, y_train, nb_epoch = 2) #, validation_split = 0.2, shuffle = True
+y_pred = model.predict(X_train)
+print(y_pred)
 
 model.save('model.h5')
